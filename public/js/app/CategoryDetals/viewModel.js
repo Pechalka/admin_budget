ef12fc40c5cf1295338ba58d5463feb2666d3e08
pechalka <luzkovskyandrey@gmail.com>
2013-12-04 18:59:06 +0300

    app_id

--- public/js/app/CategoryDetals/viewModel.js
+++ public/js/app/CategoryDetals/viewModel.js
@@ -1,72 +1,85 @@
-
 define([
-	"knockout"
-	, "text!./template.html"
-	, "bus"
-	], function(
-		ko
-		, template
-		, bus
-		){
+ "knockout"
+ , "text!./template.html"
+ , "bus"
+ ], function(
+  ko
+  , template
+  , bus
+  ){
 
-	return function(model){
-		var self = this;
-		self.templates = ['Table', 'TableJoinWith', 'TableAnual', 'TableAnualHolidays', 'Table5field', 'Table8field', 'Table2field'];
-		self.selectedTemplate = ko.observable('Table');
-		self.template_id = ko.observable();
-		self.category = null;
+ return function(model){
+  var self = this;
+  self.templates = ['Table', 'TableJoinWith', 'TableAnual', 'TableAnualHolidays', 'Table5field', 'Table8field', 'Table2field'];
+  self.selectedTemplate = ko.observable('Table');
+  self.template_id = ko.observable();
+  self.category = null;
 
-		$.get('/api/category/' + model.parent_id, function(c){
-			self.category = c;
-			self.selectedTemplate(c.template);			
-		});
-		
-		self.status = ko.observable('init');
+  $.get('/api/category/' + model.parent_id, function(c){
+   self.category = c;
+   self.selectedTemplate(c.template);   
+  });
+  
+  
+  self.apps = ko.observableArray([])
+      .extend({
+       fetch : {
+        source : '/api/app',
+        params : {
+         type : 2
+        }
+       }
+      });
+  self.apps.reload();
+  self.appId = ko.observable();
+     
+  
+  self.status = ko.observable('init');
 
-		var complete = function(){
-			self.status('complete');
-			setTimeout(function(){
-				self.status('init');
-			}, 2000)
-		}
+  var complete = function(){
+   self.status('complete');
+   setTimeout(function(){
+    self.status('init');
+   }, 2000)
+  }
 
-		self.change_template = function(){
-			if (self.status() == 'proccess') return;
+  self.change_template = function(){
+   if (self.status() == 'proccess') return;
 
-			self.status('proccess');
-			
-			self.category.template = self.selectedTemplate();
+   self.status('proccess');
+   
+   self.category.template = self.selectedTemplate();
 
-			$.put('/api/category/' + model.parent_id, self.category, complete);
-		}
+   $.put('/api/category/' + model.parent_id, self.category, complete);
+  }
 
-		self.title = ko.observable();
-		self.type = ko.observable(0);// ?????????WTF
+  self.title = ko.observable();
+  self.type = ko.observable(0);// ?????????WTF
 
-		self.add = function(){
-			$.post('/api/expenditure_type_new', { type : self.type(),  title : self.title, category_id : model.parent_id }, function(){
-				self.types.reload();
-				self.title('')
-			})
-		}
+  self.add = function(){
+   $.post('/api/expenditure_type_new', { app_id : self.appId(), type : self.type(),  title : self.title, category_id : model.parent_id }, function(){
+    self.types.reload();
+    self.title('')
+   })
+  }
 
-		self.remove = function(item){
-			$.delete('/api/expenditure_type_new/' + item.id, function(){
-				self.types.reload();		
-			})
-		}
+  self.remove = function(item){
+   $.delete('/api/expenditure_type_new/' + item.id, function(){
+    self.types.reload();  
+   })
+  }
 
-		self.types = ko.observableArray([])
-						.extend({
-							fetch : {
-								source : '/api/expenditure_type_new',
-								params : {
-									category_id : model.parent_id
-								}
-							}
-						});
-		self.types.reload();			
+  self.types = ko.observableArray([])
+      .extend({
+       fetch : {
+        source : '/api/expenditure_type_new',
+        params : {
+         category_id : model.parent_id
+        }
+       }
+      });
+  self.types.reload();   
 
-		self.template = template;
-	}
+  self.template = template;
+ }
 });
